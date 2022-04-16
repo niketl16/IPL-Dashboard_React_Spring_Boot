4cdd8648d4b5d50489534a700045faa3d4dff3cb
niketl16 <43333570+niketl16@users.noreply.github.com>
2022-04-16 08:58:39 +0530

    08 react routes & links with rest minro details

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import TeamPage from './pages/TeamPage';
 
 
 function App() {
   return (
-    <div className="TeamPage">
-     <TeamPage />
+    <div className="App">
+      
+        Main
+        <Routes>
+        <Route path="/teams/:teamName"  element={<TeamPage />}   />
+        </Routes>
+      
     </div>
   );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,34 +1,39 @@
 import { React, useEffect, useState } from "react";
 import MatchDetailCard from "../components/MatchDetailCard";
 import MatchSmallCard from "../components/MatchSmallCard";
-
+import { useParams } from "react-router-dom";
 
 export const TeamPage = () => {
     
-    const [team, setTeam] = useState();
+    const [team, setTeam] = useState(  {matches: []} );
     //setTeam({});
+    const { teamName } = useParams();
 
 
     useEffect( () => {
     const fetchMatches = async () => {
-        const response = await fetch('http://localhost:8080/team/Mumbai Indians ');
-    
+        const response = await fetch(`http://localhost:8080/team/${teamName}`);
         const data = await response.json();
         console.log(data);
         setTeam(data);
 
     };
     fetchMatches();
-},[]);
+},[teamName]);
 
+if(!team.teamName || !team){
+    return <h1>Team Not found</h1>
+}
     // {console.log(team)}
     return (
         <div className="TeamPage">
-             <h1>  nik er {team.teamName}  </h1>
+        cd
+        <h1>  nik er {team.teamName}  </h1>
             <MatchDetailCard
+                teamName = {team.teamName}
                 match = {team.matches[0]}
             />
-            {team.matches.slice(1).map(match=> <MatchSmallCard match={match} />)}
+            {team.matches.slice(1).map(match=> <MatchSmallCard teamName = {team.teamName} match={match} />)}
             
 
 
